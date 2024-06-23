7ad10ed5f4d8a7e6a39d4d0a0267f9f18d9dbbe6
Illia23 <Priymak2331@gmail.com>
2024-06-23 21:50:19 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,12 +1,16 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ContactsForm from "../../components/ContactsForm/ContactsForm";
 import ContactsList from "../../components/ContactsList/ContactsList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import { fetchContacts } from "../../redux/contacts/operatoins";
 import { useEffect } from "react";
 import styles from "./ContactsPage.module.css"; 
-
+import { selectError, selectLoading } from "../../redux/contacts/selectors";
+import Loader from '../../components/Loader/Loader';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
 const ContactsPage = () => {
+  const error = useSelector(selectError);
+  const loading = useSelector(selectLoading)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,6 +19,8 @@ const ContactsPage = () => {
 
   return (
     <div className={styles.pageContainer}>
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
       <h1 className={styles.pageTitle}>Phonebook</h1>
       <ContactsForm />
       <SearchBox />
